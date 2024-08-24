40ed04c0152f973ad8a8ad20ab104b5174e23d99
Gokilavanim06 <sruthideekonda2722@gmail.com>
2024-08-24 14:41:28 +0530

    new code

---
+++ src/main/java/com/simple/demo/CrudprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.simple.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudprojectsApplication.class, args);
+	}
+
+}
